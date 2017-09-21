1d4f050d3f0823a6ed98afcc0398d20bb8d97bcf
r12a <ishida@w3.org>
2017-09-21 14:32:23 +0100

    index, new, single page reads in js data via parameter

---
+++ gap-analysis/zh.js
@@ -0,0 +1,43 @@
+var doctitle = "Simplified Chinese and Traditional Chinese for HTML/CSS (Draft)"
+
+var compilers = "The Chinese Layout TF"
+
+var report = {
+
+'encoding': { 'status':'ok', "comments": `` },
+'fontstyle': { 'status':'na', "comments": `<p>Not applicable. See <a href="https://github.com/w3c/typography/issues/45">https://github.com/w3c/typography/issues/45</a>.</p>` },
+'glyphs': { 'status':'na', "comments": `` },
+'cursive': { 'status':'na', "comments": `` },
+'boundaries': { 'status':'ok', "comments": `` },
+'quotations': { 'status':'ok', "comments": `` },
+'numbers': { 'status':'ok', "comments": `` },
+'transforms': { 'status':'na', "comments": `` },
+'spacing': { 'status':'advanced', "comments": `` },
+'ruby': { 'status':'advanced', "comments": `` },
+'textdecoration': { 'status':'advanced', "comments": `` },
+'emphasis': { 'status':'ok', "comments": `` },
+'inlinenotes': { 'status':'', "comments": `<p>We (the Chinese Layout Task Force) don't understand what this column is about. It links to Initial letter styling, but we don't understand why "inline notes" is related to "Initial letter styling".</p>
+<p>We also don't know what "etc" means in this case. Therefore, we mark it as "unknown" for now.</p>
+<p>(If "inline notes" means <a href="https://www.w3.org/TR/jlreq/#inline_cutting_note">Inline Cutting Note (Warichu)</a>, then it should be "something prevents interoperable or effective use in webpages (0)", i.e., red.).</p>` },
+'bidi': { 'status':'na', "comments": `` },
+
+'linebreak': { 'status':'advanced', "comments": `` },
+'hyphenation': { 'status':'advanced', "comments": `<p>Chinese characters don't use hyphenation, but pinyin might use it.</p>` },
+'justification': { 'status':'advanced', "comments": `` },
+'counters': { 'status':'advanced', "comments": `` },
+'initialletter': { 'status':'basic', "comments": `` },
+'baselines': { 'status':'advanced', "comments": `` },
+'whitespace': { 'status':'advanced', "comments": `` },
+'otherpara': { 'status':'advanced', "comments": `` },
+
+'bidilayout': { 'status':'na', "comments": `` },
+'vertical': { 'status':'advanced', "comments": `` },
+'notes': { 'status':'advanced', "comments": `` },
+'pageheaders': { 'status':'', "comments": `<p>We're not quite familiar with specifications like <a href="https://drafts.csswg.org/css-break/">CSS Fragmentation</a>, <a href="https://drafts.csswg.org/css-gcpm/">CSS Generated Content for Paged Media</a>, or <a href="https://drafts.csswg.org/css-page/">CSS Paged Media</a>, so we need help from the CSS Working Group and the publishing WG/IG/BG/CGs.</p>
+<p>We mark it as "unknown" for now.</p>` },
+'morepage': { 'status':'', "comments": `` },
+
+'culture': { 'status':'', "comments": `` },
+'other': { 'status':'', "comments": `` },
+
+}
