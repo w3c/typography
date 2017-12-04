fef0eaab858c295951b0b8c9d0c5c835b6f7d188
r12a <ishida@w3.org>
2017-12-04 18:34:17 +0000

    starter template for greek

---
+++ gap-analysis/el.js
@@ -0,0 +1,76 @@
+var doctitle = "Greek for HTML/CSS (Draft)"
+
+var compilers = "Lea Verou"
+
+var report = {
+
+'encoding': { 'status':'ok', "comments": `<p></p>` },
+
+'fonts': { 'status':'ok', "comments": `<p></p>` },
+
+'fontstyle': { 'status':'ok', "comments": `<p></p>` },
+
+'glyphs': { 'status':'na', "comments": `<p></p>` },
+
+'cursive': { 'status':'na', "comments": `<p></p>` },
+
+'boundaries': { 'status':'ok', "comments": `<p></p>` },
+
+'quotations': { 'status':'ok', "comments": `<p></p>` },
+
+'numbers': { 'status':'na', "comments": `<p></p>` },
+
+'transforms': { 'status':'ok', "comments": `<p></p>` },
+
+'spacing': { 'status':'ok', "comments": `<p>.</p>` },
+
+'ruby': { 'status':'na', "comments": `<p></p>` },
+
+'textdecoration': { 'status':'ok', "comments": `<p></p>` },
+
+'emphasis': { 'status':'ok', "comments": `<p></p>` },
+
+'bidi': { 'status':'na', "comments": `<p></p>` },
+
+'otherinline': { 'status':'', "comments": `<p></p>` },
+
+
+
+
+'linebreak': { 'status':'ok', "comments": `<p></p>` },
+
+'hyphenation': { 'status':'ok', "comments": `<p></p>` },
+
+'justification': { 'status':'ok', "comments": `<p></p>` },
+
+'counters': { 'status':'ok', "comments": `<p></p>` },
+
+'initialletter': { 'status':'ok', "comments": `<p></p>` },
+
+'baselines': { 'status':'ok', "comments": `<p></p>` },
+
+'whitespace': { 'status':'na', "comments": `<p></p>` },
+
+'otherpara': { 'status':'', "comments": `<p></p>` },
+
+
+
+
+'bidilayout': { 'status':'na', "comments": `<p></p>` },
+
+'vertical': { 'status':'na', "comments": `<p></p>` },
+
+'notes': { 'status':'', "comments": `<p></p>` },
+
+'pageheaders': { 'status':'', "comments": `<p></p>` },
+
+'otherpage': { 'status':'', "comments": `<p></p>` },
+
+
+
+
+'culture': { 'status':'', "comments": `<p></p>` },
+
+'other': { 'status':'', "comments": `<p></p>` },
+
+}
